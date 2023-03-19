3fc56e54ada2fe53cd1c5aa6c52084cba15bb6c5
nataliia9909 <113458747+nataliia9909@users.noreply.github.com>
2023-03-19 21:34:43 +0300

    added corections of mentor

--- src/03-feedback.js
+++ src/03-feedback.js
@@ -1,35 +1,58 @@
 import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = "feedback-form-state";
 const formData = {};
 
-populateInput() 
-form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateInput() 
 
 
 function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-   
+    // formData[evt.target.name] = evt.target.value;
+    
+   formData.email = input.value;
+   formData.message = textarea.value;
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateInput() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
     const parcedSavedMessage = JSON.parse(savedMessage);
 
-    if (savedMessage) {
-        const formKeys = Object.keys(parcedSavedMessage);
-        formKeys.map(key => {
-            document.querySelector(`[name='${key}']`).value = parcedSavedMessage[key];
-        })
+    if (parcedSavedMessage) {
+        // const formKeys = Object.keys(parcedSavedMessage);
+        // formKeys.map(key => {
+        //     document.querySelector(`[name='${key}']`).value = parcedSavedMessage[key];
+        // })
+
+        input.value = parcedSavedMessage.email;
+        textarea.value = parcedSavedMessage.message;
     }
 }
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+
+    const formElements = evt.target.elements;
+
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+
+        if (email === "" || message === "") {
+        return alert("У формі є незаповнені поля, для відправки форми ВСІ поля повинні бути заповненими!");
+    }
+     const formData = {
+        email,
+        message,
+             }
+
+    console.log(formData);
 
     evt.target.reset();
 
